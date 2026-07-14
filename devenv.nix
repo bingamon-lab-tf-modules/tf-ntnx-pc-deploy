c581f8fed444a5fdeb5a9976c46c6f4c3515c4d2
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-14 12:21:55 +1000

    chore: globally exclude devcontainer.json from pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -123,7 +123,9 @@ in
   };
 
   git-hooks = {
-    excludes = [ ];
+    excludes = [
+      "\\.devcontainer/devcontainer\\.json$"
+    ];
     hooks = {
       actionlint.enable = true;
       action-validator.enable = true;
@@ -172,7 +174,6 @@ in
         enable = true;
         excludes = [
           "module/README.md"
-          "\\.devcontainer/devcontainer\\.json$"
         ];
       };
       # Use prettier instead.
