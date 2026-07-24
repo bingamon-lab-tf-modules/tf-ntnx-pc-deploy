d2abd826165763d66a37831e47c8b28b4292c0c6
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-24 10:00:43 +0000

    chore: Update devenv.lock (#8)
    * chore: update devenv.lock
    
    * chore: update nixfmt option in devenv.nix
    
    * chore: revert nixfmt option in devenv.nix
    
    * fix: name
    
    * chore: ignore .tars agent scratch directory

--- devenv.nix
+++ devenv.nix
@@ -64,7 +64,7 @@ let
 
 in
 {
-  name = "tf-template";
+  name = "tf-ntnx-pc-deploy";
 
   env = {
     PROJECT = config.name;
@@ -168,7 +168,7 @@ in
         };
       };
       mixed-line-endings.enable = true;
-      nixfmt-rfc-style.enable = true;
+      nixfmt.enable = true;
       pre-commit-hook-ensure-sops.enable = true;
       prettier = {
         enable = true;
